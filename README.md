8ab2b5f9de70f682c4fca94b8f2e4fb877333bbf
Thomas <thomdeveloper@gmail.com>
2017-02-19 17:06:21 -0600

    creado archivos para el modo desarrollo y produccion

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Aplicación con Django 1.10.5
+
+### Instalar Django 1.9 y paquetes
+```sh
+pip install Django==1.9 Unipath
+```
+
+### Correr la aplicación django
+```sh
+$ python manage.py runserver --settings=project.settings.local
+```
--- project/settings/base.py
+++ project/settings/base.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*
 """
 Django settings for project project.
 
@@ -11,22 +12,17 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).ancestor(3)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'm*y4$9wn)v+hs51ge%s$f&ov2+#*dhbmm-1fyi%rc74r09(!yy'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +66,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -112,9 +97,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
