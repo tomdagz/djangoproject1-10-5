9d83d23701d766b53e2b13e06679fae2a519d68b
Thomas <thomdeveloper@gmail.com>
2017-02-19 17:07:55 -0600

    archivo de produccion

---
+++ project/settings/production.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.child('db.sqlite3')#os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
